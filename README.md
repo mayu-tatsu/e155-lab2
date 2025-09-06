51ba1b278e1a52e75a6b343e0666a2df4928f555
Mayu Tatsumi <mtatsumi@hmc.edu>
2025-09-06 02:20:42 -0700

    initializing repo

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository for E155 Lab 2.
---
+++ source/impl_1/lab2_mt.sv
@@ -0,0 +1,17 @@
+// Mayu Tatsumi; mtatsumi@g.hmc.edu
+// 2025-09-06
+
+module lab2_mt(
+	input  logic       reset,
+	input  logic [3:0] onboard_s, bboard_s,
+	output logic       seg1en, seg2en,
+	output logic [4:0] led,
+	output logic [6:0] seg
+);
+
+// consider adder
+// multiplexer
+// clk to drive sev-seg outputs (HSOSC required)
+// pnp transistors on board for anode
+
+endmodule
